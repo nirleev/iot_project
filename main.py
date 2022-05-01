cdd3f06cfc2e77e5ea80f29305ce9f2695df0537
wojkos9 <wojkos9@gmail.com>
2022-05-01 17:56:57 +0200

    Basic DB interaction

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import g, Flask, request
 import sqlite3
+import os
 
-DATABASE = 'data.db'
+DATABASE = './db.sqlite3'
+
 app = Flask(__name__)
 
 state = "test"
@@ -18,17 +20,26 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+def init_db():
+    cur = get_db().cursor()
+    # TODO
+    cur.execute()
+
 
 @app.route('/')
 def hello():
     return state
 
-@app.post('/temp')
+@app.route('/temp', methods=['POST'])
 def add_data():
     global state
     data = request.get_data(as_text=True)
     state = data
     return "OK"
+
+if not os.path.exists(DATABASE):
+    with app.app_context():
+        init_db()
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 8080)
