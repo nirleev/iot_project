231a16f03da4296dcc1ef343b2c00c039635e8a2
Natalia Czyzewska <natalia0707077@gmail.com>
2022-05-01 14:11:43 -0400

    Simple data validation

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import g, Flask, request, render_template
 import sqlite3
 import os
+from datetime import datetime
 
 DATABASE = './db.sqlite3'
+TABLE_NAME = 'test'
 
 app = Flask(__name__)
 
@@ -21,28 +23,45 @@ def close_connection(exception):
 
 def init_db():
     cur = get_db().cursor()
-    # TODO
-    cur.execute('CREATE TABLE test (value text);')
+    cur.execute('CREATE TABLE {} (date datetime, temperature int);'.format(TABLE_NAME))
+    cur.execute('INSERT INTO {} (date, temperature) VALUES (\'2022-05-01 18:55:50\', 10);'.format(TABLE_NAME))
     get_db().commit()
-
 
 @app.route('/')
 def hello():
     with app.app_context():
         cur = get_db().cursor()
-        cur.execute('select value from test')
+        cur.execute('select date, temperature from test')
         data = cur.fetchall()
         cur.close()
         return render_template('test.html', data=data)
 
 @app.route('/temp', methods=['POST'])
 def add_data():
-    data = request.get_data(as_text=True)
-    with app.app_context():
-        cur = get_db().cursor()
-        cur.execute('insert into test (value) values (?)', (data,))
-        get_db().commit()
-    return "OK"
+    status = validate_data(request.form)
+    if status:
+        with app.app_context():
+            cur = get_db().cursor()
+            cur.execute('insert into test (date, temperature) values (?, ?)', (request.form.get('date'),
+                                                                               request.form.get('temperature'),))
+            get_db().commit()
+        return "OK"
+    else:
+        return "BAD REQUEST"
+
+def validate_data(req):
+    date = req.get('date')
+    temp = req.get('temperature')
+
+    if date is not None and temp is not None:
+        try:
+            int(temp)
+            datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+            return True
+        except:
+            return False
+    else:
+        return False
 
 if not os.path.exists(DATABASE):
     with app.app_context():
