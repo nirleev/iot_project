278d0b841218c069f69e6f66078246054d03c512
wojkos9 <wojkos9@gmail.com>
2022-05-01 17:37:23 +0200

    POST data test

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import g, Flask, request
+import sqlite3
 
+DATABASE = 'data.db'
 app = Flask(__name__)
+
+state = "test"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def hello():
-    return "Hello"
+    return state
+
+@app.post('/temp')
+def add_data():
+    global state
+    data = request.get_data(as_text=True)
+    state = data
+    return "OK"
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 8080)
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export FLASK_APP=main.py
+export FLASK_ENV=development
+flask run --host 0.0.0.0 --port 8080
